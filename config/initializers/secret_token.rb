7cf8d9b7a1d273f7d43a12aa5b1203ff3aa51a0f
sebtn <tonomudo@gmail.com>
2016-01-05 19:02:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instacopy::Application.config.secret_key_base = '5a79a348c0f4cea3afeb16ddbbcaf478ef61785c04dab064df0ff10d94f3e037c8ee337e70063c84baafd97399558e18151ec0284730c8c022fc435ea9cf6ec0'
